95b51d8040909dedf2af1d4d19e80a5426c587f8
xorsuyash <suyashsrivastavam87@gmail.com>
2023-02-01 22:02:34 +0530

    simple conv nn

--- autograd.py
+++ autograd.py
@@ -1,15 +1,37 @@
 import torch 
 
 x=torch.randn(3,requires_grad=True)
-print(x)
+#print(x)
 
 y=x+2
-print(y)
+#print(y)
 
 z=y*y*2
-print(z)
+#print(z)
 
 z=z.mean()
-print(z)
+#print(z)
 z.backward()
-print(x.grad)
+#print(x.grad)
+
+
+
+
+
+
+
+
+
+
+
+
+a=torch.randn(3,requires_grad=True)
+
+print(a)
+a.requires_grad_(False)
+print(a)
+
+
+#x.requires_grad_(False)
+#x.detach()
+#with torch.no_grad()
