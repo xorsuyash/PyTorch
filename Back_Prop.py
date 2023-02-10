75e63388a0567f99de9a927404a1e3f664f0cb26
xorsuyash <suyashsrivastavam87@gmail.com>
2023-02-11 04:24:17 +0530

    back_prop

---
+++ Back_Prop.py
@@ -0,0 +1,18 @@
+import torch 
+
+x=torch.tensor(1.0)
+y=torch.tensor(2.0)
+
+w=torch.tensor(1.0,requires_grad=True)
+
+y_hat=w*x
+loss=(y_hat-y)**2
+
+print(loss)
+
+#backwardpass
+
+loss.backward()
+
+print(w.grad)
+
