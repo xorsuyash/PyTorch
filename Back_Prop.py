b3b4600b8dd888d326843886e284743fa913da2b
Suyash <suyash@pop-os.localdomain>
2023-02-17 03:18:58 +0530

    learned backprop

--- Back_Prop.py
+++ Back_Prop.py
@@ -1,18 +1,12 @@
 import torch 
 
-x=torch.tensor(1.0)
-y=torch.tensor(2.0)
-
+x=torch.tensor(1.0,requires_grad=True)
 w=torch.tensor(1.0,requires_grad=True)
+y=torch.tensor(2.0,requires_grad=True)
 
 y_hat=w*x
-loss=(y_hat-y)**2
-
-print(loss)
-
-#backwardpass
+loss=(y-y_hat)**2
 
 loss.backward()
 
 print(w.grad)
-
