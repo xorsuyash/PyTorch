3c74d3111d1de4e656ef362e31ef1f5b0a040730
xorsuyash <suyashsrivastavam87@gmail.com>
2023-03-24 14:04:23 +0530

    trained nn

--- feedforward.py
+++ feedforward.py
@@ -4,7 +4,7 @@ import torchvision
 import torchvision.transforms as transform 
 import matplotlib.pyplot as plt 
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = torch.device('cuda')
 
 
 #hyperparameters 
@@ -23,7 +23,7 @@ test_dataset=torchvision.datasets.MNIST(root='./data',train=False,transform=tran
 #Data loader
 
 
-train_dataset = torch.utils.data.DataLoader(dataset=train_dataset,batch_size=batch_size,shuffle=True)
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,batch_size=batch_size,shuffle=True)
 
 test_loader = torch.utils.data.DataLoader(dataset=test_dataset,batch_size=batch_size,shuffle=False)
 
@@ -33,34 +33,81 @@ examples = iter(test_loader)
 example_data,example_target=next(examples)
 
 #for i in range(6):
-    #plt.subplot(2,3,i+1)
-    #plt.imshow(example_data[i][0],cmap='gray')
+ #   plt.subplot(2,3,i+1)
+  #  plt.imshow(example_data[i][0],cmap='gray')
 #plt.show()
 
 
-
 class NeuralNet(nn.Module):
-    def __init__(self,input_size,hidden_size,num_classes):
+    def __init__(self,input_size , hidden_size,num_classes):
         super(NeuralNet,self).__init__()
-        self.input_size=input_size
         self.l1=nn.Linear(input_size,hidden_size)
         self.relu=nn.ReLU()
         self.l2=nn.Linear(hidden_size,num_classes)
 
     def forward(self,x):
-
         out=self.l1(x)
         out=self.relu(out)
         out=self.l2(out)
-
         return out 
     
-model= NeuralNet(input_size, hidden_size,num_classes)
+model=NeuralNet(input_size,hidden_size,num_classes)
 
-criterian=nn.CrossEntropyLoss()
+criterion=nn.CrossEntropyLoss()
+optimizer=torch.optim.Adam(model.parameters(),lr=learning_rate )
+
+#training loop
+
+n_total_steps=len(train_loader)
+for epoch in range(num_epochs):
+    for i ,(image,labels) in enumerate(train_loader):
+
+        #images resize 
+
+        image=image.reshape(-1,28*28).to(device)
+        labels=labels.to(device)
+        model=model.to(device)
+
+        output=model(image)
+
+        loss=criterion(output,labels)
+        #backward
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if (i+1)%100 ==0:
+            print(f'epoch {epoch+1}/{num_epochs},step {i+1}/{n_total_steps},loss {loss.item():.4f}')
+
+#testing 
+with torch.no_grad():
+
+    n_correct=0
+    n_samples=0
+
+    for images ,labels in test_loader:
+             images=images.reshape(-1,28*28).to(device)
+
+             labels=labels.to(device)
+             model=model.to(device)
+
+             outputs=model(images)
+
+             _,prediction=torch.max(outputs,1)
+
+             n_samples +=labels.shape[0]
+             n_correct +=(prediction == labels).sum().item()
+
+    acc=100.0*n_correct/n_samples
+    print(f'accuracy = {acc}')
 
 
-parameters=next(model.parameters())
+
+
+
+
+
 
 
 
